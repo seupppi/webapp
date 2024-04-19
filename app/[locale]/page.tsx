3066765d3c366cc1fd73c152d2c0d85112db4cea
Diego Kelme <kelme.dev@pm.me>
2024-04-18 23:05:44 -0300

    v0.0.2

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -61,49 +61,8 @@ export default async function Home() {
           </div>
         </div>
 
-        <div className="Ecosystem bg-background bg-center bg-no-repeat w-screen flex flex-col items-center justify-center mt-16 py-8 xl:px-52 lg:px-16 md:px-8  z-0" id='vantagens' >
-          <div className='flex flex-col justify-evenly align-middle items-center sm:w-full w-[20rem] md:py-[3rem] py-1 place-content-center font-generalsans'>
+        
 
-            <div className='HeroSection flex flex-col text-center place-content-center'>
-              <h2 className='font-generalsans font-semibold text-xl lg:text-7xl lg:w-[27rem]'>{t('home.main.ecosystem.title')}</h2>
-              <p className="self-stretch text-white text-opacity-60 text-xl font-medium font-generalsans leading-loose lg:w-[28rem] mb-6 mt-4">{t('home.main.ecosystem.description')}</p>
-            </div>
-
-            <div className='flex flex-col md:flex-row justify-between w-full text-center space-y-5 md:space-y-0 md:space-x-5 text-md font-medium'>
-              <Link className='ImageCard flex flex-col justify-center space-y-4 hover:underline hover:decoration-solid decoration-white' href="/search">
-                <Image
-                  className="object-scale-down max-h-full drop-shadow-md m-auto rounded-md"
-                  src="/images/cards/buddysearch-card.webp"
-                  alt="A dog with a gps collar"
-                  width={360}
-                  height={260}
-                />
-
-
-              </Link>
-
-              <div className='ImageCard flex flex-col justify-center space-y-4'>
-                <Link className='ImageCard flex flex-col justify-center space-y-4 hover:underline hover:decoration-solid decoration-white' href="/safe">
-                  <Image
-                    className="object-scale-down max-h-full drop-shadow-md m-auto rounded-md"
-                    src="/images/cards/buddysafe-card.webp"
-                    alt="A cat with disabilities, that needs care and love"
-                    width={320}
-                    height={260}
-                  />
-
-                  <h3>BuddySafe</h3>
-                </Link>
-              </div>
-
-
-            </div>
-            <Button className="p-5 font-medium text-md w-fit py-6 mt-8 md:mt-10" asChild>
-              <Link href="/ecosystem">{t('home.main.ecosystem.button')}</Link>
-            </Button>
-
-          </div>
-        </div >
       </main >
 
     </>
